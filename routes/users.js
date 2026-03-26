d8125c41cdc0daefbd685dcb4378b01242e3f8d9
RafaellMoralesL <25001309@galileo.edu>
2026-03-25 20:17:43 -0600

    fix: red

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,6 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
@@ -14,41 +13,45 @@ router.post('/register', async function(req, res, next) {
   try {
     const { username, password } = req.body;
 
-    // Generacion de salt y hash utilizando bcrypt
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    // Guardar en la base de datos el usuario
     const newUser = new User({ username, password: hashedPassword });
     await newUser.save();
 
     res.status(201).json({ message: "Usuario registrado correctamente" });
-  } catch (error) {
-    console.log(error);
+} catch (error) {
+  console.log(error);
     res.status(500).json({ error: "Error en el registro", "description":error.toString() });
 }
 });
-
-
-  // Loguear usuario
-router.post('/login', async function (req, res, next){
+router.post('/login', async function(req, res, next) {
   try {
     const { username, password } = req.body;
-    
-    //  Buscar usuario en la base de datos
+
     const user = await User.findOne({ username });
     if (!user) return res.status(400).json({ error: "Usuario no encontrado" });
-    
-    // Comparar password con el hash guardado
+
+
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ error: "Contraseña incorrecta" });
 
-    // Generar un JWT para la sesion
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
 
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '7d' });
+    const isProduction = process.env.NODE_ENV === 'production';
+
+    res.cookie("habitToken", token, {
+      httpOnly: false,
+      secure: isProduction,
+      sameSite: isProduction ? 'none' : 'lax',
+      maxAge: 7 * 24 * 60 * 60 * 1000
+    });
+
+    console.log("Token generado:", token);
+    
     res.json({ message: "Inicio de sesión exitoso", token });
-  } catch (error) {
-      res.status(500).json({ error: "Error en el login", "description":error.toString() });
+} catch (error) {
+    res.status(500).json({ error: "Error en el login", "description":error.toString() });
 }
 });
 module.exports = router;
